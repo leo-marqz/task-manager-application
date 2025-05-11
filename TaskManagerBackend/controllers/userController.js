fda30bef0acb76e864f5e6c7fbafd22aaa1841a1
Elmer Marquez <leomarqz2020@gmail.com>
2025-05-11 12:49:37 -0600

    Se agrego funcionalidad de obtener usuario por el id en UserController

--- TaskManagerBackend/controllers/userController.js
+++ TaskManagerBackend/controllers/userController.js
@@ -43,7 +43,10 @@ class UserController {
     static async getUserById(req, res) {
         const { id } = req.params;
         try {
-            //
+            const user = await User.findById(id).select('-password');
+            if (!user) return res.status(404).json({ message: 'User not found' });
+            res.json(user);
+            
         } catch (error) {
             res.status(500).json({ message: 'Server error', error: error.message });
         }
