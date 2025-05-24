0f0678a2b09693aa07abd5d8d83a06fd511062dc
Elmer Marquez <leomarqz2020@gmail.com>
2025-05-23 23:13:22 -0600

    feature: Add methods for export report users and tasks - completed

--- TaskManagerBackend/routes/reportsRoutes.js
+++ TaskManagerBackend/routes/reportsRoutes.js
@@ -5,11 +5,7 @@ import { ReportController } from '../controllers/reportController.js';
 
 const router = express.Router();
 
-// Export all tasks as Excel or PDF
-// GET /api/v1/reports/export/tasks
 router.get('/export/tasks', protect, adminOnly, ReportController.exportTasksReport);
-// Export all users as Excel or PDF
-// GET /api/v1/reports/export/users
 router.get('/export/users', protect, adminOnly, ReportController.exportUsersReport);
 
 export default router;
